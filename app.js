1503d51fd38e9e91b77040b576716ab2a17bfa4d
cdallosb <cristiandaviddallosbustos@gmail.com>
2019-04-05 17:27:49 -0500

    Se incluye CORS v2

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//var cors = require('cors');
+var cors = require('cors');
 
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 
-//app.use(cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
